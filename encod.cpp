35e55154df47e5dc73d8722d16ff37934dd1517f
MohulinV <72401571+MohulinV@users.noreply.github.com>
2020-10-06 16:08:16 +0300

    Update encod.cpp

--- encod.cpp
+++ encod.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main()
 {
     setlocale(0, "RUS");
-    
+    /* Зміна на сайті*/
     char buff[26]={'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
     int j;
     
